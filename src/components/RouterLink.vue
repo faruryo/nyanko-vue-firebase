21468ba2e50747e00a78b4f0271aecd3b8198ce4
Ryo Murata <faru@RyonoMac-mini.local>
2019-01-05 18:49:04 +0900

    fix RouterLink

--- src/components/RouterLink.vue
+++ src/components/RouterLink.vue
@@ -1,10 +1,10 @@
 <template>
-  <v-list-tile :to="{path: linkname}">
+  <v-list-tile :to="{path: getRoute(linkname)['path']}">
     <v-list-tile-action>
-      <v-icon>{{ getRoute( linkname, 'icon' ) }}</v-icon>
+      <v-icon>{{ getRoute(linkname)['meta']['icon'] }}</v-icon>
     </v-list-tile-action>
     <v-list-tile-content>
-      <v-list-tile-title>{{getRoute( linkname, 'name' )}}</v-list-tile-title>
+      <v-list-tile-title>{{ getRoute(linkname)['name'] }}</v-list-tile-title>
     </v-list-tile-content>
   </v-list-tile>
 </template>
@@ -16,10 +16,10 @@ export default {
     linkname: String
   },
   methods: {
-    getRoute (name, key) {
+    getRoute (name) {
       for (var i = 0; i < this.$router.options.routes.length; i++) {
         if (this.$router.options.routes[i].name === name) {
-          return this.$router.options.routes[i].meta[key]
+          return this.$router.options.routes[i]
         }
       }
     }
